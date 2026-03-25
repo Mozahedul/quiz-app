f7ce44a438c57ef001f8a22845a948e3f1dda9f2
Mozahedul <mozahed001@gmail.com>
2026-03-26 00:35:16 +0600

    Add html, css, js, nodejs, expressjs, & MongoDB

--- quiz.js
+++ quiz.js
@@ -1,21 +1,46 @@
-const fetchQuizData = async () => {
+const menuList = [
+  {
+    name: "HTML",
+    url: "https://gist.githubusercontent.com/Mozahedul/23b62956155e9d8fdcebb6680d43393f/raw/html.json",
+  },
+  {
+    name: "CSS",
+    url: "https://gist.githubusercontent.com/Mozahedul/2c540573bfc92e92598a0b184eb28c51/raw/css.json",
+  },
+  {
+    name: "JavaScript",
+    url: "https://gist.githubusercontent.com/Mozahedul/a741404beafa87f1d8b21c8121a50ae2/raw/quiz.json",
+  },
+  {
+    name: "NodeJS",
+    url: "https://gist.githubusercontent.com/Mozahedul/6dda022d2314f30d22498a286b227467/raw/nodejs.json",
+  },
+  {
+    name: "ExpressJS",
+    url: "https://gist.githubusercontent.com/Mozahedul/95de74139dd79e84eb4146045daf30e8/raw/expressjs.json",
+  },
+  {
+    name: "MongoDB",
+    url: "https://gist.githubusercontent.com/Mozahedul/d5d674b7b36da86f32a0ae65ccc2b30c/raw/mongodb.json",
+  },
+];
+
+// Fetch data from API
+const fetchQuizData = async url => {
   try {
-    const url =
-      "https://gist.githubusercontent.com/Mozahedul/a741404beafa87f1d8b21c8121a50ae2/raw/quiz.json";
-    const response = await fetch(`${url}?cachebust=${Date.now()}`, {
+    const response = await fetch(url, {
       cache: "no-cache",
     });
     const data = await response.json();
     return data;
   } catch (error) {
-    console.error("Error fetching quiz data:", error);
+    console.error("Error fetching quiz data:", error.message);
   }
 };
 
-const quizData = await fetchQuizData();
-console.log("fetch data => ", quizData);
+const quizData = await fetchQuizData(menuList[2].url);
 
-const questions = [...quizData].sort(() => Math.random() - 0.5);
+let questions = [...quizData].sort(() => Math.random() - 0.5);
 let currentQuestion = 0;
 let score = 0;
 let timer;
@@ -28,23 +53,67 @@ const timerEl = document.getElementById("timer");
 const resultEl = document.getElementById("result");
 const quizBox = document.getElementById("quiz-box");
 
+// Load Quiz menu
+function loadMenu() {
+  // Load menu list
+  const quizMenu = document.getElementById("quizMenu");
+  quizMenu.innerHTML = "";
+
+  menuList.forEach(list => {
+    const menuBtn = document.createElement("button");
+    menuBtn.classList.add("menu-btn");
+    menuBtn.textContent = list.name;
+    quizMenu.appendChild(menuBtn);
+
+    menuBtn.addEventListener("click", async () => {
+      questions = await fetchQuizData(list.url);
+
+      if (!questions || questions.length === 0) return;
+
+      loadQuizTitle(list.name);
+
+      document.querySelector(".menu-btn.active")?.classList.remove("active");
+      menuBtn.classList.add("active");
+
+      currentQuestion = 0;
+      score = 0;
+      loadQuestion();
+    });
+
+    if (list.name === "JavaScript") {
+      menuBtn.classList.add("active");
+    }
+  });
+}
+
+// ✅ Load quiz title
+function loadQuizTitle(title) {
+  const quizMenu = document.getElementById("quizMenu");
+  // Remove existing h1 if it exists
+  if (quizMenu?.nextElementSibling?.tagName === "H1") {
+    quizMenu.nextElementSibling.remove();
+  }
+  const quizTitleElm = document.createElement("h1");
+  quizTitleElm.classList.add("quizTitle");
+  quizTitleElm.textContent = `${title} Quizzes`;
+  quizMenu.after(quizTitleElm);
+}
+
+loadQuizTitle("JavaScript");
+
+// Load questions
 function loadQuestion() {
   clearInterval(timer);
-  const current = quizData[currentQuestion];
-  timeLeft = current.duration;
+  const q = questions[currentQuestion];
+  timeLeft = q.duration;
   updateTimer();
   timer = setInterval(countdown, 1000);
-
-  // show time left
-  // quizData.forEach((option,index) => {
-  //   timeLeft = option.duration;
-  // });
 
   timerEl.style.display = "block";
   nextBtn.style.display = "none";
   optionsEl.style.display = "block";
   questionEl.style.display = "block";
-  const q = questions[currentQuestion];
+
   questionEl.textContent = `Q${currentQuestion + 1}. ${q.question}`;
 
   // Remove the previous options before loading new ones
@@ -55,14 +124,12 @@ function loadQuestion() {
     const btn = document.createElement("button");
     btn.addEventListener("click", () => selectAnswer(index, true));
     btn.classList.add("option-btn");
-    const opationsLetter =
-      index === 0 ? "A" : index === 1 ? "B" : index === 2 ? "C" : "D";
+    const opationsLetter = ["A", "B", "C", "D"][index];
     btn.innerHTML = `
     <span class="letter">${opationsLetter}</span> 
     <span class="option-text">${option}</span>
     `;
     optionsEl.appendChild(btn);
-
     nextBtn.style.display = "none";
   });
 }
@@ -88,12 +155,13 @@ function formatTime(seconds) {
 }
 
 function updateTimer() {
-  timerEl.textContent =
-    timeLeft > 0 ? `Time Left: ${formatTime(timeLeft)}` : "Time's up!";
-  timerEl.style.width = "50%";
-  timerEl.style.margin = "10px auto";
+  timerEl.innerHTML =
+    timeLeft > 0
+      ? `<strong>⌚ Time Left:</strong> ${formatTime(timeLeft)}`
+      : "😢 Time's up!";
+  timerEl.style.display = "inline-block";
   timerEl.style.borderRadius = "40px";
-  timerEl.style.padding = "8px 0";
+  timerEl.style.padding = "5px 15px";
 
   timerEl.style.backgroundColor = timeLeft <= 15 ? "#e74c3c" : "#141414";
 }
@@ -165,4 +233,5 @@ function resetQuiz() {
   loadQuestion();
 }
 
+loadMenu();
 loadQuestion();
